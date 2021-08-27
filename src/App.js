7e8fce94cdc7c1b09ed9455d9b9f2913b565a4ac
Justin Wayne Wilson <39743876+WilsonJustinWayne@users.noreply.github.com>
2021-08-26 19:19:52 -0500

    Lets try this again

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from "./components/HomePage/HomePage";
 import LeagueCreationPage from "./components/LeagueCreationPage/LeagueCreationPage";
